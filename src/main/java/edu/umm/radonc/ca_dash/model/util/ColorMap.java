fa775f4cffebe40f3ba598cfeff85a2c3ae2332c
mmcgrath <mmcgrath@HGB-GJ10-PC01.umms.umm.edu>
2014-11-04 12:09:09 -0500

    Update hospital names in colormap

--- src/main/java/edu/umm/radonc/ca_dash/model/util/ColorMap.java
+++ src/main/java/edu/umm/radonc/ca_dash/model/util/ColorMap.java
@@ -19,10 +19,12 @@ public class ColorMap {
             synchronized (ColorMap.class) {
                 map = new TreeMap<>();
                 map.put("All", new String[]{"EA1300","F53800","DE5A0B","F57F00","EA9600"});
-                map.put("University of Maryland", new String[]{"8C3130","994D2D","A34230","A33065","992D95"});
+                map.put("University of Maryland Medical Center", new String[]{"8C3130","994D2D","A34230","A33065","992D95"});
                 map.put("Central Maryland Radiation Oncology", new String[]{"E0AB5D","ED8556","F7A25A","F7C75B","EDCE56"});
+                map.put("Upper Chesapeake Health System", new String[]{"4984D0","5142DD","4662E7","46B6E7","42D7DD"});
+                map.put("Baltimore Washington Medical Center", new String[]{"2C2A29", "393632", "433F3B", "433D3B", "393232"});
+                map.put("University of Maryland", new String[]{"8C3130","994D2D","A34230","A33065","992D95"});
                 map.put("Upper Chesapeake Health", new String[]{"4984D0","5142DD","4662E7","46B6E7","42D7DD"});
-                map.put("Baltimore Washington Medical Center", new String[]{"2C2A29", "393632", "433F3B", "433D3B", "393232"});         
             }
         }
         return map;
